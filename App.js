803d7174ad477b29641f554f8975e31d9239cfa7
Gyeomyi Hwang <austin.hwang.dev@gmail.com>
2023-05-06 21:48:32 +0900

    Feat: Finish developing application
    - Add code for requesting permission for user location
    - Add code for getting user location data
    - Add code for getting weather information of user location
    - Add .env enviroment variable
    - Install dependencies

--- App.js
+++ App.js
@@ -1,13 +1,75 @@
-import { ScrollView, StyleSheet, Dimensions, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import {
+	ScrollView,
+	StyleSheet,
+	Dimensions,
+	Text,
+	View,
+	ActivityIndicator,
+} from "react-native";
+import * as Location from "expo-location";
 
 // 스크린의 사이즈 가져오기
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
+// API KEY from .env
+import { API_KEY } from "@env";
+
 export default function App() {
+	// 현재 위치
+	const [city, setCity] = useState("Loading...");
+
+	// 날짜별 (총 5일) 날씨 정보
+	const [days, setDays] = useState([]);
+
+	// Granted 여부 확인
+	const [ok, setOk] = useState(true);
+
+	const getWeather = async () => {
+		// requestForegroundPermissionsAsync: 앱 사용중에만 위치 사용 권한 허용 요청
+		const { granted } = await Location.requestForegroundPermissionsAsync();
+		if (!granted) {
+			setOk(false);
+		}
+		// 사용자 latitude, longitude 가져오기
+		const {
+			coords: { latitude, longitude },
+		} = await Location.getCurrentPositionAsync({ accuracy: 5 });
+
+		// 가져온 latitude, longitude를 통해 사용자 위치(도시명) 가져오기
+		const location = await Location.reverseGeocodeAsync(
+			{
+				latitude,
+				longitude,
+			},
+			{ useGoogleMaps: false }
+		);
+
+		// 가져온 도시 정보를 통해 city state에 저장
+		setCity(location[0].city);
+
+		// 가져온 latitude, longitude를 통해 날씨 정보 fetch
+		const response = await fetch(
+			`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+		);
+		const json = await response.json();
+		setDays(
+			// 각 day의 날씨 정보 중 0시의 날씨 정보만 days state에 저장
+			json.list.filter((weather) => {
+				if (weather.dt_txt.includes("00:00:00")) {
+					return weather;
+				}
+			})
+		);
+	};
+
+	useEffect(() => {
+		getWeather();
+	});
 	return (
 		<View style={styles.container}>
 			<View style={styles.city}>
-				<Text style={styles.cityName}>Seoul</Text>
+				<Text style={styles.cityName}>{city}</Text>
 			</View>
 			<ScrollView
 				showsHorizontalScrollIndicator={false}
@@ -15,22 +77,33 @@ export default function App() {
 				horizontal
 				contentContainerStyle={styles.weather}
 			>
-				<View style={styles.day}>
-					<Text style={styles.temp}>27</Text>
-					<Text style={styles.description}>Sunny</Text>
-				</View>
-				<View style={styles.day}>
-					<Text style={styles.temp}>27</Text>
-					<Text style={styles.description}>Sunny</Text>
-				</View>
-				<View style={styles.day}>
-					<Text style={styles.temp}>27</Text>
-					<Text style={styles.description}>Sunny</Text>
-				</View>
-				<View style={styles.day}>
-					<Text style={styles.temp}>27</Text>
-					<Text style={styles.description}>Sunny</Text>
-				</View>
+				{/* 날씨 정보가 fetch 완료되기 전 */}
+				{days.length === 0 ? (
+					<View style={styles.day}>
+						<ActivityIndicator
+							color="white"
+							style={{
+								marginTop: 10,
+							}}
+							size="large"
+						/>
+					</View>
+				) : (
+					days.map(function (day, index) {
+						// 날씨 정보가 fetch완료 되었을 때
+						return (
+							<View key={index} style={styles.day}>
+								<Text style={styles.temp}>
+									{parseFloat(day.main.temp).toFixed(1)}
+								</Text>
+								<Text style={styles.description}>{day.weather[0].main}</Text>
+								<Text style={styles.tinyDescription}>
+									{day.weather[0].description}
+								</Text>
+							</View>
+						);
+					})
+				)}
 			</ScrollView>
 		</View>
 	);
@@ -59,5 +132,9 @@ const styles = StyleSheet.create({
 	description: {
 		marginTop: -30,
 		fontSize: 60,
+	},
+	tinyDescription: {
+		marginTop: 10,
+		fontSize: 20,
 	},
 });
